2f4cb15a30e65dec8a54b8490db19103d95f2bdd
coolweb <jordenschristophe@gmail.com>
2016-08-17 07:33:54 +0200

    add missing files

---
+++ CollectionneurRecettes.Addon/CollectionneurRecettes.Addon/CrossCutting/ILoggerService.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="ILoggerService.cs" company="No company">
+// 2016 GNU license
+// </copyright>
+// <author>Jordens Christophe</author>
+
+namespace CollectionneurRecettes.Addon.CrossCutting
+{
+    /// <summary>
+    /// The logger service interface.
+    /// </summary>
+    public interface ILoggerService
+    {
+        /// <summary>
+        /// Logs the information.
+        /// </summary>
+        /// <param name="log">The log.</param>
+        void LogInformation(string log);
+
+        /// <summary>
+        /// Logs the verbose.
+        /// </summary>
+        /// <param name="log">The log.</param>
+        void LogVerbose(string log);
+
+        /// <summary>
+        /// Logs the warning.
+        /// </summary>
+        /// <param name="log">The log.</param>
+        void LogWarning(string log);
+
+        /// <summary>
+        /// Logs the error.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void LogError(string message);
+    }
+}
---
+++ CollectionneurRecettes.Addon/CollectionneurRecettes.Addon/CrossCutting/LoggerService.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="LoggerService.cs" company="No company">
+// 2016 GNU license
+// </copyright>
+// <author>Jordens Christophe</author>
+
+namespace CollectionneurRecettes.Addon.CrossCutting
+{
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Log with system diagnostic into the trace source.
+    /// </summary>
+    /// <seealso cref="CollectionneurRecettes.Addon.CrossCutting.ILoggerService" />
+    public class LoggerService : ILoggerService
+    {
+        /// <summary>
+        /// The trace source.
+        /// </summary>
+        private TraceSource trace = new TraceSource("CollectionneurRecettes.Addon");
+
+        /// <inheritdoc />
+        public void LogError(string message)
+        {
+            this.trace.TraceEvent(TraceEventType.Error, 2, message);
+        }
+
+        /// <inheritdoc />
+        public void LogInformation(string log)
+        {
+            this.trace.TraceInformation(log);
+        }
+
+        /// <inheritdoc />
+        public void LogVerbose(string log)
+        {
+            this.trace.TraceEvent(TraceEventType.Verbose, 1, log);
+        }
+
+        /// <inheritdoc />
+        public void LogWarning(string log)
+        {
+            this.trace.TraceEvent(TraceEventType.Warning, 2, log);
+        }
+    }
+}
